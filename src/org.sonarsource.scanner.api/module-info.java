380c02440ce2d3789e646350c006e3a8034c201d
Julien HENRY <henryju@yahoo.fr>
2016-10-07 11:18:35 +0200

    Try to fix gson

--- src/org.sonarsource.scanner.api/module-info.java
+++ src/org.sonarsource.scanner.api/module-info.java
@@ -6,6 +6,7 @@ module org.sonarsource.scanner.api {
   requires java.sql;
   requires java.management;
   requires java.desktop;
+  requires jdk.unsupported;
   exports org.sonarsource.scanner.api;
   exports org.sonarsource.scanner.api.internal.batch;
   
